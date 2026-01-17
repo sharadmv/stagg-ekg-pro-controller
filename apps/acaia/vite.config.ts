0dd60b724b458c39deab4064edf3a5038dad09d1
Sharad Vikram <sharad.vikram@gmail.com>
2026-01-16 22:02:37 -0800

    fix(acaia): use absolute path for sdk alias and add @types/node

--- apps/acaia/vite.config.ts
+++ apps/acaia/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
     base: '/coffee-tools/acaia/',
     resolve: {
         alias: {
-            '@coffee-tools/acaia-sdk': '../../packages/acaia-sdk/src/index.ts'
+            '@coffee-tools/acaia-sdk': path.resolve(__dirname, '../../packages/acaia-sdk/src/index.ts')
         }
     },
     server: {
